b54c36e71d552043f1be5e4dabc9bb951638e6ed
wajdirezek <rezekwajdi@gmail.com>
2024-12-25 10:55:56 +0100

    API

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ListUsers from './Components/ListUsers';
+import NavUsers from './Components/NavUser';
+
+function App() {
+  return (
+    <div>
+      <NavUsers/>
+      <ListUsers/>
+    </div>
+  );
+}
+
+export default App;
